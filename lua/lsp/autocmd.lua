4575057376ccb73e1dda2ded9bd45b346bf1a2c9
kira <kira@fanruan.com>
2023-09-22 15:10:21 +0800

    update

--- lua/lsp/autocmd.lua
+++ lua/lsp/autocmd.lua
@@ -1,10 +1,12 @@
 lvim.autocommands = {
+  -- auto open minimap
   {
     "BufWinEnter", -- see `:h autocmd-events`
     {              -- this table is passed verbatim as `opts` to `nvim_create_autocmd`
       command = ":lua MiniMap.open()"
     },
   },
+  -- auto format when save
   {
     "User",
     {
@@ -17,4 +19,30 @@ lvim.autocommands = {
       end,
     },
   },
+  -- auto close nvim-tree when last file
+  {
+    "QuitPre",
+    {
+      callback = function()
+        local tree_wins = {}
+        local floating_wins = {}
+        local wins = vim.api.nvim_list_wins()
+        for _, w in ipairs(wins) do
+          local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+          if bufname:match("NvimTree_") ~= nil then
+            table.insert(tree_wins, w)
+          end
+          if vim.api.nvim_win_get_config(w).relative ~= '' then
+            table.insert(floating_wins, w)
+          end
+        end
+        if 1 == #wins - #floating_wins - #tree_wins then
+          -- Should quit, so we close all invalid windows.
+          for _, w in ipairs(tree_wins) do
+            vim.api.nvim_win_close(w, true)
+          end
+        end
+      end
+    }
+  }
 }
