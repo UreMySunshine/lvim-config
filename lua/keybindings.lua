98c41e3370e1fe96ba3679e2c5025babfa303868
kira <kira@fanruan.com>
2023-09-27 14:40:05 +0800

    update

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,18 +1,3 @@
--- Telescope find_files
-lvim.builtin.which_key.mappings["tf"] = {
-  "<cmd>Telescope find_files<cr>",
-  "Telescope find_files",
-}
--- Telescope projects
-lvim.builtin.which_key.mappings["tp"] = {
-  "<cmd>Telescope projects<cr>",
-  "Telescope projects",
-}
--- Telescope live_grep
-lvim.builtin.which_key.mappings["tg"] = {
-  "<cmd>Telescope live_grep<cr>",
-  "Telescope lvie_grep",
-}
 -- rust
 lvim.builtin.which_key.mappings["C"] = {
   name = "Rust",
--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -1,6 +1,6 @@
 lvim.lsp.buffer_mappings.normal_mode["<leader>sw"] = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>",
   "Search current word" }
-lvim.lsp.buffer_mappings.normal_mode["<leader>sp"] = {
+lvim.lsp.buffer_mappings.normal_mode["<leader>se"] = {
   "<cmd>lua require('spectre').open_file_search({select_word=true})<cr>", "Search on current file" }
 lvim.lsp.buffer_mappings.visual_mode["<leader>sw"] = { "<esc><cmd>lua require('spectre').open_visual()<cr>",
   "Search on current word" }
