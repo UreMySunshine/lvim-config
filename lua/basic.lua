493fe47ddf3ac35c0541c957c1fdc184a3c9b968
kira <kira@fanruan.com>
2023-09-28 15:31:19 +0800

    update filter

--- lua/basic.lua
+++ lua/basic.lua
@@ -3,7 +3,7 @@ vim.opt.relativenumber = true
 -- auto update buffer
 vim.opt.autoread = true
 -- nvim-tree filter
-lvim.builtin.nvimtree.setup.filters.custom = { "\\.git", ".DS_Store" }
+lvim.builtin.nvimtree.setup.filters.custom = { "\\.git$", ".DS_Store" }
 -- autoformat
 lvim.format_on_save = {
   enabled = true,
