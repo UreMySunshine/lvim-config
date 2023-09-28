e465f9d9df7bdf3b8c5d950a00d5d4831584e242
kira <kira@fanruan.com>
2023-09-28 15:09:32 +0800

    update popupview keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -29,3 +29,5 @@ lvim.builtin.which_key.mappings["t3"] = {
 lvim.builtin.which_key.mappings["t4"] = {
   "<cmd>4ToggleTerm size=15 direction=horizontal<cr>", "Split horizontal"
 }
+-- move cursor to popup view
+lvim.keys.normal_mode["gg"] = "<C-w><C-w>"
