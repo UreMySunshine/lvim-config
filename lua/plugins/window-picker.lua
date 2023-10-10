e32258db9e8826d11aff17fb5d42b2b96ed5ced9
kira <kira@fanruan.com>
2023-10-10 11:18:55 +0800

    update

--- lua/plugins/window-picker.lua
+++ lua/plugins/window-picker.lua
@@ -1,38 +1,45 @@
 return {
   "s1n7ax/nvim-window-picker",
-  version = "1.*",
+  branch = 'release/2.0',
+  event = 'VeryLazy',
   config = function()
-    require("window-picker").setup({
-      autoselect_one = true,
-      include_current = false,
-      filter_rules = {
-        -- filter using buffer options
-        bo = {
-          -- if the file type is one of following, the window will be ignored
-          filetype = { "neo-tree", "neo-tree-popup", "notify", "quickfix" },
-
-          -- if the buffer type is one of following, the window will be ignored
-          buftype = { "terminal" },
-        },
-      },
-      other_win_hl_color = "#e35e4f",
-    })
     -- example mappings you can place in some other place
     -- An awesome method to jump to windows
     local picker = require('window-picker')
 
+    local config = {
+      hint = 'floating-big-letter',
+
+      filter_rules = {
+        -- when there is only one window available to pick from, use that window
+        -- without prompting the user to select
+        autoselect_one = true,
+
+        -- whether you want to include the window you are currently on to window
+        -- selection or not
+        include_current_win = false,
+
+        -- filter using buffer options
+        bo = {
+          -- if the file type is one of following, the window will be ignored
+          filetype = { "notify", "quickfix", "minimap", "noice", "fidget", "unknown" },
+
+          -- if the buffer type is one of following, the window will be ignored
+          buftype = {},
+        },
+      },
+    }
+
+    picker.setup(config)
+
     vim.keymap.set("n", ",w", function()
-      local picked_window_id = picker.pick_window({
-        include_current_win = true
-      }) or vim.api.nvim_get_current_win()
-      vim.api.nvim_set_current_win(picked_window_id)
+      local window = picker.pick_window(config) or vim.api.nvim_get_current_win()
+      vim.api.nvim_set_current_win(window)
     end, { desc = "Pick a window" })
 
     -- Swap two windows using the awesome window picker
     local function swap_windows()
-      local window = picker.pick_window({
-        include_current_win = false
-      })
+      local window = picker.pick_window(config)
       local target_buffer = vim.fn.winbufnr(window)
       -- Set the target window to contain current buffer
       vim.api.nvim_win_set_buf(window, 0)
