09e8e312fb2b1ed76a0937bbf05bf9974e69055b
kira <kira@fanruan.com>
2023-10-10 16:36:40 +0800

    update

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -22,9 +22,11 @@ return {
       }
     })
     lvim.builtin.which_key.mappings["la"] = { "<cmd>Lspsaga code_action<cr>", "Lspsaga Code Action", noremap = true }
-    lvim.builtin.which_key.mappings["lr"] = { "<cmd>Lspsaga rename<cr>", "Lspsaga Rename" }
-    lvim.builtin.which_key.mappings["lj"] = { "<cmd>Lspsaga diagnostic_jump_next<cr>", "Lspsaga Next Diagnostic" }
-    lvim.builtin.which_key.mappings["lk"] = { "<cmd>Lspsaga diagnostic_jump_prev<cr>", "Lspsaga Prev Diagnostic" }
-    vim.keymap.set("n", "gf", "<cmd>Lspsaga finder<cr>", { silent = true })
+    lvim.builtin.which_key.mappings["lr"] = { "<cmd>Lspsaga rename<cr>", "Lspsaga Rename", noremap = true }
+    lvim.builtin.which_key.mappings["lj"] = { "<cmd>Lspsaga diagnostic_jump_next<cr>", "Lspsaga Next Diagnostic",
+      noremap = true }
+    lvim.builtin.which_key.mappings["lk"] = { "<cmd>Lspsaga diagnostic_jump_prev<cr>", "Lspsaga Prev Diagnostic",
+      noremap = true }
+    vim.keymap.set("n", "gf", "<cmd>Lspsaga finder<cr>", { silent = true, desc = "Lspsaga finder" })
   end,
 }
