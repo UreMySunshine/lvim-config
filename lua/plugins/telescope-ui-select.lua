3416e0991a32253c4305ab705636eb93daa2a706
kira <kira@fanruan.com>
2023-10-13 15:24:28 +0800

    update

--- lua/plugins/telescope-ui-select.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'nvim-telescope/telescope-ui-select.nvim',
-  lazy = true,
-  enabled = lvim.builtin.telescope.active,
-  config = function()
-    require("telescope").load_extension("ui-select")
-  end
-}
