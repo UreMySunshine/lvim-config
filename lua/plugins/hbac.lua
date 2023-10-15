fe4612d59128c57c3589d42f6a9939733e1f89e2
mio <hu1051261239@gmail.com>
2023-10-15 20:55:00 +0800

    update

--- lua/plugins/hbac.lua
+++ lua/plugins/hbac.lua
@@ -1,9 +1,11 @@
 return {
   'axkirillov/hbac.nvim',
+  lazy = true,
+  event = "User FileOpened",
   config = function()
     require("hbac").setup({
       autoclose                  = true, -- set autoclose to false if you want to close manually
-      threshold                  = 10, -- hbac will start closing unedited buffers once that number is reached
+      threshold                  = 10,   -- hbac will start closing unedited buffers once that number is reached
       close_command              = function(bufnr)
         vim.api.nvim_buf_delete(bufnr, {})
       end,
