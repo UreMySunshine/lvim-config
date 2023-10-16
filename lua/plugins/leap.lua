7df9f46686e9c58c175ee8b7388c85417003f64b
kira <kira@fanruan.com>
2023-10-16 17:23:26 +0800

    update

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,9 +1,10 @@
 return {
   "ggandor/leap.nvim",
   lazy = true,
-  event = { "CursorHold", "CursorHoldI" },
+  keys = { "s", "S" },
   name = "leap",
   config = function()
+    require("leap").opts.highlight_unlabeled_phase_one_targets = true
     require("leap").add_default_mappings()
   end,
 }
