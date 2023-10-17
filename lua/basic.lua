4dde8149d9a0a97df9b1892904f5c2b744658f32
kira <kira@fanruan.com>
2023-10-17 18:28:57 +0800

    update

--- lua/basic.lua
+++ lua/basic.lua
@@ -3,7 +3,9 @@ vim.opt.relativenumber = true
 -- auto update buffer
 vim.opt.autoread = true
 vim.cmd [[au CursorHold * checktime]]
--- nvim-tree filter
+-- nvim-tree
+lvim.builtin.nvimtree.setup.view.width = 35
+lvim.builtin.nvimtree.setup.git.enable = false
 lvim.builtin.nvimtree.setup.filters.custom = { "\\.git$", ".DS_Store" }
 -- autoformat
 lvim.format_on_save = {
@@ -12,3 +14,4 @@ lvim.format_on_save = {
 }
 -- cmd-cmp
 lvim.builtin.cmp.cmdline.enable = true
+-- breadcrumbs
