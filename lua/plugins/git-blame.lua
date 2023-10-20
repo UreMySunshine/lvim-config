f12ae6009bac69356431ba29573380596a577994
kira <kira@fanruan.com>
2023-10-20 14:24:23 +0800

    update plugin

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -2,8 +2,4 @@ return {
   "f-person/git-blame.nvim",
   lazy = true,
   event = "BufRead",
-  config = function()
-    vim.cmd "highlight default link gitblame SpecialComment"
-    require("gitblame").setup { enabled = false }
-  end,
 }
